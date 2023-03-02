40cbbac074cb5ce099a04f441e698f57be2dc5c8
hgarciaospina <henrygarciaospina@gmail.com>
2023-03-02 10:18:48 -0500

    1.- Datos comunes

---
+++ TypeData/src/data/Data.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Data {
+    public static void main(String[] args) {
+        int id = 100;
+        long mass =  178823411L;
+        double weight = 80.5d;
+        boolean isActive = true;
+        String message = "Congratulations";
+
+        System.out.println("Tipo de dato int :" + id);
+        System.out.println("Tipo de dato long :" + mass);
+        System.out.println("Tipo de dato double :" + weight);
+        System.out.println("Tipo de dato boolean :" + isActive);
+        System.out.println("Tipo de dato String :" + message);
+    }
+}
