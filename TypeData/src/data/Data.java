83c2b22b3f5b7c82f7a20615de3c77e4f99de9a3
hgarciaospina <henrygarciaospina@gmail.com>
2023-03-02 16:02:06 -0500

    complemento datos

--- TypeData/src/data/Data.java
+++ TypeData/src/data/Data.java
@@ -2,16 +2,36 @@ package data;
 
 public class Data {
     public static void main(String[] args) {
-        int id = 100;
-        long mass =  178823411L;
-        double weight = 80.5d;
-        boolean isActive = true;
-        String message = "Congratulations";
 
+        /* 1.Númericos */
+
+        // 1.1 Enteros
+        byte height = 127;
+        short distance = 1500;
+        int id = 10000000;
+        long mass =  178823411L;
+
+        // 1.2 decimales
+        float PI = 3.1416f;
+        double weight = 80.5d;
+
+        /* Booleano */
+        boolean isActive = true;
+        boolean isFinalized = false;
+
+        /* Texto */
+        String message = "Congratulations";
+        char active = 'Y';
+
+        System.out.println("Tipo de dato  byte :" + height);
+        System.out.println("Tipo de dato  short :" + distance);
         System.out.println("Tipo de dato int :" + id);
         System.out.println("Tipo de dato long :" + mass);
+        System.out.println("Tipo de dato float :" + PI);
         System.out.println("Tipo de dato double :" + weight);
         System.out.println("Tipo de dato boolean :" + isActive);
+        System.out.println("Tipo de dato boolean :" + isFinalized);
+        System.out.println("Tipo de dato char :" + active);
         System.out.println("Tipo de dato String :" + message);
     }
 }
